9e60a7e083b092138d22ff54811507a303fd529d
Julia <morkajulia@gmail.com>
2024-03-27 20:16:08 +0100

    Changed default start and end year constants.

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 RAW_DATA_URL = "https://danepubliczne.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/dobowe/opad/"
-DEFAULT_START_YEAR = 2018
-DEFAULT_END_YEAR = 2019
+DEFAULT_START_YEAR = 1990
+DEFAULT_END_YEAR = 2020
 INPUT_DATA_PATH = "data/input/"
 INTERIM_DATA_PATH = "data/interim/"
 HTTP_REQUEST_TIMEOUT = 10
